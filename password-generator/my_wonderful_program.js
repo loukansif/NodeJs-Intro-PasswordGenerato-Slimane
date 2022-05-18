7045d74a55cf86f715f8fcce6b5513c9b5ec7685
slami <lamislimane@gmail.com>
2022-05-18 11:58:21 +0200

    add function generatePassword

--- password-generator/my_wonderful_program.js
+++ password-generator/my_wonderful_program.js
@@ -1,1 +1,14 @@
 console.log("Node rocks!");
+
+function generatePassword(length) {
+  let result = "";
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+console.log(generatePassword(5));
